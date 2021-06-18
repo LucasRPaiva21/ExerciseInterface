1bf179bee0f19393ea1e922b25c6ad5b47fafb3c
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-18 02:51:30 -0300

    Projeto Finalizado

--- ComInterfaceTax/Program.cs
+++ ComInterfaceTax/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ComInterfaceTax.Entities;
+using ComInterfaceTax.Services;
+using System;
+using System.Globalization;
 
 namespace ComInterfaceTax
 {
@@ -6,7 +9,26 @@ namespace ComInterfaceTax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data!");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
---
+++ ComInterfaceTax/Services/ITaxService.cs
@@ -0,0 +1,7 @@
+﻿namespace ComInterfaceTax.Services
+{
+    interface ITaxService
+    {
+        double Tax(double amount);
+    }
+}
