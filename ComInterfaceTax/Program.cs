5052e5109a882b85c90f73f871f83301980b6806
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-18 02:17:30 -0300

    Projeto Iniciado

---
+++ ComInterfaceTax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComInterfaceTax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
